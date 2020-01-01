d38cc5986ed707009c8f676c566e073b729b4c28
jpan0831 <hpan0831@gmail.com>
2020-01-01 23:34:04 +0800

    Style repo with css modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import headerStyles from './header.module.scss'
+
 export default function Header() {
   return (
-    <header>
-      <h1>Jay Pan</h1>
+    <header calassName={headerStyles.header}>
+      <h1>
+        <Link to="/" className={headerStyles.title}>
+          Jay Pan
+        </Link>
+      </h1>
       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
+        <ul className={headerStyles.navList}>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+          <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
         </ul>
       </nav>
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import Header from './header';
 import Footer from './footer';
+import headerStyles from './layout.module.scss';
+import "../styles/index.scss" 
 
 export default function Layout(props) {
   return (
-    <div>
-      <Header />
-      {props.children}
+    <div className={headerStyles.container}>
+      <div className={headerStyles.content}>
+        <Header />
+        {props.children}
+      </div>
       <Footer />
     </div>
   );
