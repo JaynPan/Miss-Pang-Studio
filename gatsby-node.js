20e8c507677ddbff9e804bc818628bb65b5a8869
jpan0831 <hpan0831@gmail.com>
2020-03-30 00:44:33 +0800

    Create single work node

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,9 +3,17 @@ const path = require("path")
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve("./src/templates/blog.js")
+  const workTemplate = path.resolve("./src/templates/work.js")
   const res = await graphql(`
     query {
       allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+      allContentfulWork {
         edges {
           node {
             slug
@@ -19,6 +27,15 @@ module.exports.createPages = async ({ graphql, actions }) => {
     createPage({
       component: blogTemplate,
       path: `/blog/${edge.node.slug}`,
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+  res.data.allContentfulWork.edges.forEach(edge => {
+    createPage({
+      component: workTemplate,
+      path: `/work/${edge.node.slug}`,
       context: {
         slug: edge.node.slug,
       },
---
+++ src/templates/work.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Head from "../components/head"
+
+export const query = graphql`
+  query($slug: String!) {
+    contentfulWork(slug: {eq: $slug} ) {
+      title
+    }
+  }
+`
+
+export default function Work(props) {
+  const { title } = props.data.contentfulWork
+  return (
+    <Layout>
+      <Head title={title} />
+      <h1>{title}</h1>
+    </Layout>
+  )
+}
