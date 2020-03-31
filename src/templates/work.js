fabf1becdb4cf54f833169c6da65448b10fc8d26
jpan0831 <hpan0831@gmail.com>
2020-04-01 00:28:27 +0800

    Fetch single work content from Contentful

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
@@ -8,16 +9,32 @@ export const query = graphql`
   query($slug: String!) {
     contentfulWork(slug: { eq: $slug }) {
       title
+      content {
+        json
+      }
     }
   }
 `
 
 export default function Work(props) {
-  const { title } = props.data.contentfulWork
+  const { title, content } = props.data.contentfulWork
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        if (node.data.target.fields) {
+          const alt = node.data.target.fields.title["en-US"]
+          const { url } = node.data.target.fields.file["en-US"]
+          return <img alt={alt} src={url} />
+        }
+      },
+    },
+  }
+
   return (
     <Layout>
       <Head title={title} />
       <h1>{title}</h1>
+      {documentToReactComponents(content.json, options)}
     </Layout>
   )
 }
