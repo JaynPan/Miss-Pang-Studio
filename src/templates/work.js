0745ce45e1f777f7b3d3fd0fc39a5746e5a4d545
Jiehen Pan <lb0104672@gmail.com>
2020-04-05 22:54:28 +0800

    Use rich text images with fluid gatsby-image

--- src/templates/work.js
+++ src/templates/work.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import RichTextRenderer from "../components/contentful_rich_text_renderer"
 
 import Layout from "../components/layout"
 import Head from "../components/head"
@@ -19,24 +19,13 @@ export const query = graphql`
 
 export default function Work(props) {
   const { title, content } = props.data.contentfulWork
-  const options = {
-    renderNode: {
-      "embedded-asset-block": node => {
-        if (node.data.target.fields) {
-          const alt = node.data.target.fields.title["en-US"]
-          const { url } = node.data.target.fields.file["en-US"]
-          return <img alt={alt} src={url} />
-        }
-      },
-    },
-  }
 
   return (
     <Layout>
       <Head title={title} />
       <article className="work-article">
         <h1 className="title">{title}</h1>
-        {documentToReactComponents(content.json, options)}
+        <RichTextRenderer richTextJson={content.json} />
       </article>
     </Layout>
   )
